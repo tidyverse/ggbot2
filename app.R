7f0cb114cf4620c0bd8ef1f6ebc84aca7de55a3c
parmsam <parmartsam@gmail.com>
2025-10-26 19:51:23 -0400

    Add initial implementation of diagrambot voice interface for publishing purpose

---
+++ app.R
@@ -0,0 +1,10 @@
+# Install diagrambot from GitHub if not already installed
+if (!require("diagrambot", quietly = TRUE)) {
+  remotes::install_github("parmsam/canvasbot")
+}
+
+# Load the diagrambot package
+library(diagrambot)
+
+# Launch the voice interface
+diagrambot_voice()
